6319c0c73718dc329e0e17562e4871a060f12e49
Dholglas Rodrigues <dholglasrodrigues@gmail.com>
2025-10-24 18:00:21 +0100

    🛠️ app/urls.py -> Updated routes; index now uses car_view 🔴 car_manage/templates/cars.html 🛠️ car_manage/templates/index.html -> Replaced with cars.html content; new car listing and UI features 🛠️ car_manage/views.py -> car_view uses index.html; index function renamed to skeleton; removed cars.html usage

--- car_manage/views.py
+++ car_manage/views.py
@@ -4,23 +4,24 @@ from car_manage.models import Car # Use the ORM from Django to access the DB via
 ############### USER VIEW - ###################
 
 def car_view(request):
+# search = request.GET.get('search')
+  db_cars = Car.objects.all() # Get all the info from the db cars
   
-  db_cars = Car.objects.all() # get all the info from the db cars
-  
-# filtered_cars = Car.objects.filter(brand__name = '') # Filter by a specific value withou using the ID
+# filtered_cars = Car.objects.filter(brand__name = 'example') # Filter by a specific value withou using the ID
+# filtered_cars = Car.objects.filter(model__contains = 'search') # Filter by a specific string
   
   
   
   return render(
     request,
-    'cars.html',
+    'index.html',
     {'db_cars' : db_cars} 
     ) # use the default request and the template page you want to get
 
-def index(request):
+def skeleton(request):
   return render(
     request,
-    'index.html'
+    'skeleton.html'
   ) # default page just for test purposes 
   
   
