19d94ec54bcee41c20697499132744ed9bb4efdb
Dholglas Rodrigues <dholglasrodrigues@gmail.com>
2025-10-25 11:56:50 +0100

    🛠️ app/settings.py -> Added template directory path 🛠️ app/urls.py -> Cleaned comments, clarified car_view 🛠️ car_manage/views.py -> Cars now ordered by model, 🛠️ car_manage/views.py -> Improved case-insensitive search 🔴 car_manage/templates/skeleton.html 🛠️ car_manage/templates/index.html -> Removed blank line 🛠️ app/__pycache__/settings.cpython-313.pyc -> Binary updated 🛠️ app/__pycache__/urls.cpython-313.pyc -> Binary updated 🛠️ car_manage/__pycache__/views.cpython-313.pyc -> Binary updated

--- app/settings.py
+++ app/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['app/templates'], # Base template
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- car_manage/views.py
+++ car_manage/views.py
@@ -4,19 +4,20 @@ from car_manage.models import Car # Use the ORM from Django to access the DB via
 ############### USER VIEW - ###################
 
 def car_view(request):
-  db_cars = Car.objects.all() # Get all the info from the db cars
+  db_cars = Car.objects.all().order_by('model') # Get all the info from the db cars and order by model
   search = request.GET.get('search') # custom search via url
   
-  #if is being user, show specific model. Else render all cars
+  #if 'search' is being used, show specific model. Else render all cars in the db
   if search:
-    db_cars = db_cars.filter(model__contains = search)
+    db_cars = db_cars.filter(model__icontains = search)
   
-  
+################# RETURN THE CONTENT ##################  
   return render(
     request,
     'index.html',
     {'db_cars' : db_cars} 
     ) # use the default request and the template page you want to get
+
 
 def skeleton(request):
   return render(
